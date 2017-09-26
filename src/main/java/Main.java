e1a560bb4c261022eba362432c1bc155c4533ed1
krisztian <alt.krisztian@gmail.com>
2017-09-27 00:00:07 +0200

    Add new products to database, create new product images

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -58,15 +58,27 @@ public class Main {
         supplierDataStore.add(cbs);
         Supplier whiteStarLine = new Supplier("White Star Line", "Ship building");
         supplierDataStore.add(whiteStarLine);
-
+        Supplier emi = new Supplier("EMI Music", "CD and LP publishing etc.");
+        supplierDataStore.add(emi);
+        Supplier sportMuseum = new Supplier("Sport Museum", "Sport equipment");
+        supplierDataStore.add(sportMuseum);
+        Supplier hungary = new Supplier("State of Hungary", "Our beautiful homeland");
+        supplierDataStore.add(hungary);
+        Supplier louvre = new Supplier("Louvre", "Museum in Paris, France");
+        supplierDataStore.add(louvre);
+        Supplier pentagon = new Supplier("Pentagon", "The headquarters of the US Department of Defense");
+        supplierDataStore.add(pentagon);
 
 
         //setting up a new product category
         ProductCategory movies = new ProductCategory("Movies", "Entertainment", "Vehicles, weapons and other famous objects from great movies.");
         productCategoryDataStore.add(movies);
 
-        ProductCategory historical = new ProductCategory("Historical", "Article", "Tangible memories from great persons.");
+        ProductCategory historical = new ProductCategory("Historical", "Article", "Tangible memories from emperors, generals etc.");
         productCategoryDataStore.add(historical);
+
+        ProductCategory famous = new ProductCategory("Famous Artifacts", "Article", "Tangible memories from popular musicians, athletes etc.");
+        productCategoryDataStore.add(famous);
 
 
         //setting up products and printing it
@@ -74,17 +86,32 @@ public class Main {
         productDataStore.add(new Product("Bud Spencer's pan", 20, "USD", "Old tool from az old friend. Old tool from az old friend.", movies, columbia, "bud_pan.jpg"));
         productDataStore.add(new Product("The last soap from Fight Club", 25, "USD", "Be clean. Be a fighter. Be a weapon.", movies, fox, "soap.jpg"));
         productDataStore.add(new Product("The Ring", 3000, "USD", "One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them.", movies, newLineCinema, "ring.jpg"));
-        productDataStore.add(new Product("AT-ST", 479, "USD", "Good old vehicle from the dark side.", movies, lucas, "atst.jpg"));
+        productDataStore.add(new Product("Carbonite Han Solo", 500, "USD", "This cold piece of carbonite was Jabba's favorite knick-knackery.", movies, lucas, "hansolo.jpg"));
         productDataStore.add(new Product("Jockey Ewing's whiskey cup", 200, "USD", "A glass cup from the famous oil tycoon.", movies, cbs, "whiskeycup.jpg"));
         productDataStore.add(new Product("Chewbacca's crossbow", 89, "USD", "Dependable weapon from a good guy.", movies, lucas, "chewbaccabow.jpg"));
         productDataStore.add(new Product("Terminator's endoskeleton", 2000, "USD", "Your new personal coach and trainer", movies, orion, "terminator.jpg"));
-        productDataStore.add(new Product("Death Star", 4000, "USD", "Your new flat. Your new flat. Your new flat. Your new flat.", movies, lucas, "deathstar.jpg"));
+        productDataStore.add(new Product("AT-ST", 479, "USD", "Good old vehicle from the dark side.", movies, lucas, "atst.jpg"));
         productDataStore.add(new Product("The Helmet of Darth Vader", 500, "USD", "Black hat from the dark side.", movies, lucas, "vaderhelmet.jpg"));
         productDataStore.add(new Product("R2D2", 400, "USD", "Faithful personal assistant. Faithful personal assistant.", movies, lucas, "r2d2.jpg"));
         productDataStore.add(new Product("Millennium Falcon", 1500, "USD", "Good vehicle instead of a dull car.", movies, lucas, "falcon.jpg"));
         productDataStore.add(new Product("Titanic Wreck", 10000, "USD", "A sad wreck in the deep sea. A sad wreck in the deep sea. A sad wreck in the deep sea.", historical, whiteStarLine, "titanic.jpg"));
-        productDataStore.add(new Product("The dagger, which killed Iulius Caesar", 89, "USD", "This weapon was used by Brutus too. This weapon was used by Brutus too.", historical, uffizi, "dagger.jpg"));
-
+        productDataStore.add(new Product("The Dagger of the Killers of Iulius Caesar", 89, "USD", "This weapon was used by Brutus too. This weapon was used by Brutus too.", historical, uffizi, "dagger.jpg"));
+        productDataStore.add(new Product("Death Star", 4000, "USD", "Your new flat. Your new flat. Your new flat. Your new flat.", movies, lucas, "deathstar.jpg"));
+        productDataStore.add(new Product("The Holy Right", 3000, "USD", "The blissful hand of King Saint Stephen. The blissful hand of King Saint Stephen.", historical, hungary, "holyright.jpg"));
+        productDataStore.add(new Product("The Horse of Attila the Hun", 3000, "USD", "Original and entire skeleton, it has every bones.", historical, hungary, "attila.jpg"));
+        productDataStore.add(new Product("The Holy Crown", 3000, "USD", "Crown jewel from the ancient times of Hungary.", historical, hungary, "crown.jpg"));
+        productDataStore.add(new Product("Elvis Jacket", 1230, "USD", "Elvis went on the stage in this jacket many times.", famous, emi, "elvis.jpg"));
+        productDataStore.add(new Product("Donald Trump's necktie", 90, "USD", "Trump wore this necktie when he discussed some problems with Kim Jong-un.", famous, pentagon, "trump.jpg"));
+        productDataStore.add(new Product("Anita Gorbicz's first ball", 200, "USD", "Gorbicz played her first handball match with this ball. She was only 6 years old.", famous, sportMuseum, "gorbicz.jpg"));
+        productDataStore.add(new Product("Neymar's shoes", 170, "USD", "Neymar scored 4 goals against Mozambique in this special sport equipment.", famous, louvre, "neymar.jpg"));
+        productDataStore.add(new Product("Mohamed Ali's boxing gloves", 800, "USD", "Ali won against a lot of opponents in this gloves.", famous, sportMuseum, "ali.jpg"));
+        productDataStore.add(new Product("Zsolt Erdei's boxing gloves", 100, "USD", "Erdei won against a lot of opponents in this gloves.", famous, sportMuseum, "erdei.jpg"));
+        productDataStore.add(new Product("Federer's tennis racket", 200, "USD", "Federer won his last US Open with this racket.", famous, sportMuseum, "federer.jpg"));
+        productDataStore.add(new Product("The piano of John Lennon", 500, "USD", "Lennon played Imagine on this instrument.", famous, emi, "lennon.jpg"));
+        productDataStore.add(new Product("The shirt of Ferenc Puskas", 820, "USD", "Puskas scored 3 goals against England in this jersey.", famous, sportMuseum, "puskas.jpg"));
+        productDataStore.add(new Product("Mona Lisa", 3000, "USD", "Leonardo's original painting, it's a very exclusive offer.", historical, louvre, "monalisa.jpg"));
+        productDataStore.add(new Product("Sebastian Vettel's helmet", 330, "USD", "Vettel won 4 Formula-1 GP with this helmet.", famous, sportMuseum, "vettel.jpg"));
+        productDataStore.add(new Product("Brian May's Red Special", 1000, "USD", "A guitar made by the famous member of Queen, he played in this instrument for example We Will Rock You.", famous, emi, "brianmay.jpg"));
 
     }
 
