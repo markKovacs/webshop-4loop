2bf9a26152253dfe68f7da6063e6928ad5dcfd31
Peter Juhasz <jpeter82@gmail.com>
2017-09-28 13:20:12 +0200

    Initialize Config

---
+++ src/main/java/com/codecool/shop/Config.java
@@ -0,0 +1,12 @@
+package com.codecool.shop;
+
+public class Config {
+
+    public static final String EMAIL_FROM = "info@4loop.hu";
+    public static final String EMAIL_SENDER_HOST = "localhost";
+    public static final String SMTP_HOST = "";
+
+
+
+
+}
