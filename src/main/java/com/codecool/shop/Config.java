b1a00b2fa3a5056b84ffff9f8fd5701f4345d702
Peter Juhasz <jpeter82@gmail.com>
2017-09-28 22:30:59 +0200

    Add admin log path to Config

--- src/main/java/com/codecool/shop/Config.java
+++ src/main/java/com/codecool/shop/Config.java
@@ -2,6 +2,7 @@ package com.codecool.shop;
 
 public class Config {
 
+    // Email settings
     public static final String EMAIL_FROM = "";
     public static final String EMAIL_SENDER_HOST = "";
     public static final String SMTP_HOST = "";
@@ -9,8 +10,8 @@ public class Config {
     public static final String SMTP_PASS = "";
     public static final String SMTP_PORT = "";
 
-
-
-
+    // Log folder path - no slash at the end!
+    public static final String ORDER_LOG_FOLDER = "src/main/orders";
+    public static final String ADMIN_LOG_FOLDER = "src/main/logs";
 
 }
