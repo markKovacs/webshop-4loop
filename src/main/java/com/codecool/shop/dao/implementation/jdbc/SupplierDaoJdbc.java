1ccb010d4060f9442ec1c2e7280db4572ae17ff3
Peter Juhasz <jpeter82@gmail.com>
2017-10-10 16:23:31 +0200

    Start to implement find and getAll methods in SupplierDaoJdbc

--- src/main/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbc.java
@@ -1,8 +1,13 @@
 package com.codecool.shop.dao.implementation.jdbc;
 
+import com.codecool.shop.DB;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SupplierDaoJdbc implements SupplierDao {
@@ -14,6 +19,24 @@ public class SupplierDaoJdbc implements SupplierDao {
 
     @Override
     public Supplier find(int id) {
+
+        try (DB db = new DB();
+            PreparedStatement stmt = db.getPreparedStatement("SELECT ?, ? FROM suppliers WHERE id = ?;")
+        ){
+            stmt.setString(1, "name");
+            stmt.setString(2, "description");
+            stmt.setInt(3, id);
+            ResultSet resultSet = stmt.executeQuery();
+            System.out.println(resultSet.getFetchSize());
+            if(resultSet.next()){
+                Supplier supplier = new Supplier(resultSet.getString("name"),
+                        resultSet.getString("description"));
+                System.out.println(supplier.toString());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
@@ -24,6 +47,24 @@ public class SupplierDaoJdbc implements SupplierDao {
 
     @Override
     public List<Supplier> getAll() {
+
+        List resultList = new ArrayList();
+
+        try (DB db = new DB();
+             PreparedStatement stmt = db.getPreparedStatement("SELECT id, name, description FROM suppliers;")
+        ){
+            ResultSet resultSet = stmt.executeQuery();
+            while (resultSet.next()){
+                resultList.add(new Supplier(
+                        resultSet.getString("name"),
+                        resultSet.getString("description")));
+            }
+            System.out.println(resultList);
+            return resultList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
