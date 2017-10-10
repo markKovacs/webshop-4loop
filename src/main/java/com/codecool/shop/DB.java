047df4190da9666ff3bf93f7a51b2889bb91bcb7
Peter Juhasz <jpeter82@gmail.com>
2017-10-10 16:20:47 +0200

    Create DB connection class

--- src/main/java/com/codecool/shop/DB.java
+++ src/main/java/com/codecool/shop/DB.java
@@ -1,32 +1,111 @@
 package com.codecool.shop;
 
 import java.sql.*;
+import java.util.List;
 
-public class DB {
+public class DB implements AutoCloseable {
 
-    private static final String DATABASE =
+    private static String DATABASE =
             "jdbc:" + Config.DB_TYPE + "://" + Config.HOST + ":" + Config.PORT + "/" + Config.DB_NAME;
-    private static final String DB_USER = Config.USER;
-    private static final String DB_PASSWORD = Config.PASSWORD;
+    private static String DB_USER = Config.USER;
+    private static String DB_PASSWORD = Config.PASSWORD;
+    private Connection connection;
 
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-                DATABASE,
-                DB_USER,
-                DB_PASSWORD);
+    public DB() {
+        if (Config.USE_PRODUCTION_DB) {
+            DATABASE =
+                    "jdbc:" + Config.DB_TYPE + "://" + Config.HOST + ":" + Config.PORT + "/" + Config.DB_NAME;
+            DB_USER = Config.USER;
+            DB_PASSWORD = Config.PASSWORD;
+        } else {
+            DATABASE =
+                    "jdbc:" + Config.TEST_DB_TYPE + "://" + Config.TEST_HOST + ":" + Config.TEST_PORT + "/" + Config.TEST_DB_NAME;
+            DB_USER = Config.TEST_USER;
+            DB_PASSWORD = Config.TEST_PASSWORD;
+        }
     }
 
-    public void executeQuery(String query) {
-        try (Connection connection = getConnection();
-             Statement statement =connection.createStatement();
-        ){
-            statement.execute(query);
+    private Connection getConnection() {
+        try{
+            return DriverManager.getConnection(
+                    DATABASE,
+                    DB_USER,
+                    DB_PASSWORD);
+        }catch (SQLException em){
+            em.printStackTrace();
+        }
 
+        return null;
+    }
+
+    public PreparedStatement getPreparedStatement(String query) {
+        try {
+            this.connection = getConnection();
+            this.connection.setAutoCommit(false);
+            PreparedStatement pstmt = this.connection.prepareStatement(query);
+            System.out.println("NOW WE SEND pstmt FROM getPreparedStatement!");
+            return pstmt;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /*public int executeDML(PreparedStatement pstmt) {
+        int affectedRows = -1;
+
+        try {
+            affectedRows = pstmt.executeUpdate();
+            this.connection.commit();
         } catch (SQLException e) {
+            try{
+                this.connection.rollback();
+            } catch (SQLException er) {
+                er.printStackTrace();
+            }
+        } finally{
+            try{
+                if(pstmt!=null)
+                    pstmt.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+            try {
+                if(this.connection != null) {
+                    this.connection.close();
+                }
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+
+        return affectedRows;
+    }
+
+
+    public ResultSet executeDQL(PreparedStatement pstmt) {
+
+        try  {
+            ResultSet resultSet = pstmt.executeQuery();
+            //this.connection.commit();
+            return resultSet;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }*/
+
+    @Override
+    public void close() throws SQLException {
+        try {
+            if(this.connection != null) {
+                this.connection.close();
+            }
+        } catch (SQLException e){
             e.printStackTrace();
         }
     }
 
-    // TODO: generic queries could be implemented here
 
 }
