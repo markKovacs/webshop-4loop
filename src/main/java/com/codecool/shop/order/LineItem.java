19af9bd5fc4e26f13ce05bf2f8ef4233423fdd8b
Peter Juhasz <jpeter82@gmail.com>
2017-10-12 19:19:24 +0200

    Add currency and order_id to LineItem

--- src/main/java/com/codecool/shop/order/LineItem.java
+++ src/main/java/com/codecool/shop/order/LineItem.java
@@ -2,35 +2,41 @@ package com.codecool.shop.order;
 
 import com.codecool.shop.model.Product;
 
+import java.util.Currency;
+
 public class LineItem {
     //private static int idCounter = 0;
     //private int id;
 
     //private Product product;
+    private int orderId;
     private int productId;
     private String productName;
     private String productImage;
 
     private int quantity;
     private float actualPrice;
+    private Currency currency;
     private float subTotalPrice;
 
-    public LineItem(int productId, String productName, String productImage, int quantity, float actualPrice) {
+    public LineItem(int orderId, int productId, String productName, String productImage, int quantity, float actualPrice, Currency currency) {
         //this.id = idCounter;
         //idCounter++;
         //this.id = lineItemId;
+        this.orderId = orderId;
         this.productId = productId;
         this.productName = productName;
         this.productImage = productImage;
         this.quantity = quantity;
         this.actualPrice = actualPrice;
         this.subTotalPrice = quantity * actualPrice;
+        this.currency = currency;
     }
 
-    public void changeQuantity(int amount) {
+    /*public void changeQuantity(int amount) {
         this.quantity += amount;
         this.subTotalPrice += amount * actualPrice;
-    }
+    }*/
 
     public float changeQuantityToValue(int quantity) {
         this.quantity = quantity;
@@ -82,8 +88,20 @@ public class LineItem {
         return subTotalPrice;
     }
 
-    public void setSubTotalPrice(int subTotalPrice) {
+    public void setSubTotalPrice(float subTotalPrice) {
         this.subTotalPrice = subTotalPrice;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
     }
 
     @Override
