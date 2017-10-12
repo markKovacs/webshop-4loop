249da702dc3376d29adbc9036b2b9bdc56ea85d8
Peter Juhasz <jpeter82@gmail.com>
2017-10-12 11:26:04 +0200

    Add new method to DAO interface

--- src/main/java/com/codecool/shop/dao/OrderDao.java
+++ src/main/java/com/codecool/shop/dao/OrderDao.java
@@ -3,12 +3,15 @@ package com.codecool.shop.dao;
 import com.codecool.shop.order.Order;
 import com.codecool.shop.order.Status;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface OrderDao {
 
-    void add(Order order);
-    Order find(int id);
+    Order createNewOrder(int userId);
+    // void add(Order order);
+    Order findByID(int id);
+    Order findOpenByUserId(int userId);
     void remove(int id);
 
     List<Order> getAll();
