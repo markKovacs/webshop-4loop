03ef84859010afb04c738bb9defd6587cf0bb871
Peter Juhasz <jpeter82@gmail.com>
2017-10-12 19:23:24 +0200

    Implement interface changes in OrderDaoJdbc

--- src/main/java/com/codecool/shop/dao/implementation/jdbc/OrderDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/jdbc/OrderDaoJdbc.java
@@ -2,6 +2,7 @@ package com.codecool.shop.dao.implementation.jdbc;
 
 import com.codecool.shop.DB;
 import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.Supplier;
 import com.codecool.shop.order.LineItem;
 import com.codecool.shop.order.Order;
@@ -12,9 +13,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.OptionalInt;
+import java.util.*;
 
 public class OrderDaoJdbc implements OrderDao {
 
@@ -22,12 +21,12 @@ public class OrderDaoJdbc implements OrderDao {
     @Override
     public Order createNewOrder(int userId) {
 
+        // TODO: update log_filename!!!
+
         Order order = null;
 
         String query = "INSERT INTO orders (user_id) " +
-                       "VALUES (?)" +
-                       "RETURNING id;";
-
+                       "VALUES (?)";
 
         try (DB db = new DB();
              PreparedStatement stmt = db.getPreparedStatement(query, OptionalInt.of(Statement.RETURN_GENERATED_KEYS))
@@ -106,43 +105,46 @@ public class OrderDaoJdbc implements OrderDao {
             stmt.setInt(1, userId);
             ResultSet resultSet = stmt.executeQuery();
 
-            if(resultSet != null) {
-                List<LineItem> items = new ArrayList<>();
-                float totalPrice = 0;
-                Status status;
+            List<LineItem> items = new ArrayList<>();
+            float totalPrice = 0;
+            Status status;
 
-                while (resultSet.next()) {
+            while (resultSet.next()) {
 
-                    int quantity = resultSet.getInt("quantity");
-                    float actual_price = resultSet.getFloat("actual_price");
-                    totalPrice += quantity * actual_price;
+                int quantity = resultSet.getInt("quantity");
+                float actualPrice = resultSet.getFloat("actual_price");
+                totalPrice += quantity * actualPrice;
 
-                    items.add(new LineItem(
-                        resultSet.getInt("product_id"),
-                        resultSet.getString("product_name"),
-                        resultSet.getString("image_filename"),
-                        quantity,
-                        actual_price
-                    ));
-                }
+                items.add(new LineItem(
+                    resultSet.getInt("order_id"),
+                    resultSet.getInt("product_id"),
+                    resultSet.getString("product_name"),
+                    resultSet.getString("image_filename"),
+                    quantity,
+                    actualPrice,
+                    Currency.getInstance(resultSet.getString("currency"))
+                ));
 
-                switch (resultSet.getString("order_status")) {
-                    case "reviewed":
-                        status = Status.REVIEWED;
-                        break;
-                    case "checked":
-                        status = Status.CHECKEDOUT;
-                        break;
-                    case "paid":
-                        status = Status.PAID;
-                        break;
-                    default:
-                        status = Status.NEW;
-                }
+                if (resultSet.last()) {
 
-                order = new Order(
+                    switch (resultSet.getString("order_status")) {
+                        case "reviewed":
+                            status = Status.REVIEWED;
+                            break;
+                        case "checked":
+                            status = Status.CHECKEDOUT;
+                            break;
+                        case "paid":
+                            status = Status.PAID;
+                            break;
+                        default:
+                            status = Status.NEW;
+                            break;
+                    }
+
+                    order = new Order(
                         resultSet.getInt("order_id"),
-                        resultSet.getInt("user_id"),
+                        userId,
                         status,
                         items,
                         resultSet.getDate("closed_date"),
@@ -159,8 +161,10 @@ public class OrderDaoJdbc implements OrderDao {
                         resultSet.getString("shipping_zip"),
                         resultSet.getString("shipping_address"),
                         resultSet.getString("orderlog_filename")
-                        );
+                    );
+                }
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -168,26 +172,178 @@ public class OrderDaoJdbc implements OrderDao {
         return order;
     }
 
+
     @Override
     public void remove(int id) {
 
     }
+
 
     @Override
     public List<Order> getAll() {
         return null;
     }
 
+
     @Override
     public List<Order> getBy(Status status) {
         return null;
     }
+
 
     @Override
     public List<Order> getAllPaid(int userId) {
         return null;
     }
 
+    @Override
+    public void addLineItemToOrder(Order order, Product product, int quantity) {
+
+        String query = "INSERT INTO lineitems (product_id, order_id, quantity, actual_price, currency) " +
+                       "VALUES (?, ?, ?, ?, ?);";
+
+        try (DB db = new DB();
+             PreparedStatement stmt = db.getPreparedStatement(query.trim())
+        ) {
+            stmt.setInt(1, product.getId());
+            stmt.setInt(2, order.getId());
+            stmt.setInt(3, quantity);
+            stmt.setFloat(4, product.getDefaultPrice());
+            stmt.setString(5, product.getDefaultCurrency().toString());
+
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows > 0){
+                System.out.println("New product added to database.");
+            } else {
+                System.out.println("New product addition failed.");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void updateLineItemInOrder(Order order, Product product, int quantity) {
+
+        String query = "UPDATE lineitems " +
+                       "SET quantity = ? " +
+                       "WHERE product_id = ? AND order_id = ?;";
+
+        try (DB db = new DB();
+             PreparedStatement stmt = db.getPreparedStatement(query)
+        ) {
+            stmt.setInt(1, quantity);
+            stmt.setInt(2, product.getId());
+            stmt.setInt(3, order.getId());
+
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows > 0){
+                System.out.println("New product added to database.");
+            } else {
+                System.out.println("New product addition failed.");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public LineItem findLineItemInCart(int productId, Order order) {
+
+        String query = "SELECT l.product_id," +
+                       "       l.order_id," +
+                       "       l.quantity," +
+                       "       l.actual_price," +
+                       "       l.currency," +
+                       "       p.name product_name," +
+                       "       p.image_filename " +
+                       "FROM lineitems l " +
+                       "JOIN products p ON p.id = l.product_id " +
+                       "WHERE order_id = ? AND product_id = ?;";
+
+        try (DB db = new DB();
+             PreparedStatement stmt = db.getPreparedStatement(query.trim())
+        ){
+            stmt.setInt(1, order.getId());
+            stmt.setInt(2, productId);
+
+            ResultSet resultSet = stmt.executeQuery();
+
+            if (resultSet.next()) {
+                return new LineItem(
+                        order.getId(),
+                        productId,
+                        resultSet.getString("product_name"),
+                        resultSet.getString("image_filename"),
+                        resultSet.getInt("quantity"),
+                        resultSet.getFloat("actual_price"),
+                        Currency.getInstance(resultSet.getString("currency"))
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void addLineItemToCart(LineItem lineItem, Order order) {
+
+        String query = "INSERT INTO lineitems (product_id, order_id, quantity, actual_price, currency, product_name, image_filename) " +
+                       "VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+        try (DB db = new DB();
+             PreparedStatement stmt = db.getPreparedStatement(query)
+        ) {
+            stmt.setInt(1, lineItem.getProductId());
+            stmt.setInt(2, order.getId());
+            stmt.setInt(3, lineItem.getQuantity());
+            stmt.setFloat(4, lineItem.getActualPrice());
+            stmt.setString(5, lineItem.getCurrency().toString());
+            stmt.setString(6, lineItem.getProductName());
+            stmt.setString(7, lineItem.getProductImage());
+
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Creating order failed!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void changeQuantity(LineItem lineItem, int quantity) {
+        String query = "UPDATE lineitems " +
+                       "SET quantity = (quantity + ?) " +
+                       "WHERE product_id= ? AND order_id = ?;";
+
+        try (DB db = new DB();
+             PreparedStatement stmt = db.getPreparedStatement(query)
+        ) {
+            stmt.setInt(1, quantity);
+            stmt.setInt(2, lineItem.getProductId());
+            stmt.setInt(3, lineItem.getOrderId());
+
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Creating order failed!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
     /*@Override
