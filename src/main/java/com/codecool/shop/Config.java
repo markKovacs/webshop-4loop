cdbc08680aaf328c507fec833e44a2b86964af72
Mark Kovacs <kovacsmark89@gmail.com>
2017-10-16 12:16:29 +0200

    Config file changed and removed unnecessary dependencies

---
+++ src/main/java/com/codecool/shop/Config.java
@@ -0,0 +1,34 @@
+package com.codecool.shop;
+
+public class Config {
+
+    // Email settings
+    public static final String EMAIL_FROM = "";
+    public static final String EMAIL_SENDER_HOST = "";
+    public static final String SMTP_HOST = "";
+    public static final String SMTP_USER = "";
+    public static final String SMTP_PASS = "";
+    public static final String SMTP_PORT = "";
+
+    // Log folder path - no slash at the end!
+    public static final String ORDER_LOG_FOLDER = "src/main/orders";
+    public static final String ADMIN_LOG_FOLDER = "src/main/logs";
+
+    public static final boolean USE_DB = true;
+    public static final boolean USE_PRODUCTION_DB = true;
+
+    public static final String DB_TYPE = "postgres";
+    public static final String HOST = "localhost";
+    public static final String PORT = "5432";
+    public static final String DB_NAME = "";
+    public static final String USER = "";
+    public static final String PASSWORD = "";
+
+    public static final String TEST_DB_TYPE = "";
+    public static final String TEST_HOST = "";
+    public static final String TEST_PORT = "";
+    public static final String TEST_DB_NAME = "";
+    public static final String TEST_USER = "";
+    public static final String TEST_PASSWORD = "";
+
+}
